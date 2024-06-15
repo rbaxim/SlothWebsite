42109ab7b736e7f4434c3f36c906e373eec74cc8
rbaxim <142934208+rbaxim@users.noreply.github.com>
2024-06-15 18:44:56 -0400

    React framework added

---
+++ react/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>
+          This is a basic React template.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
