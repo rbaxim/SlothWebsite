cef0d66b5ede9657261e1c2dc46af7d9b0087097
rbaxim <luc.r.diverse@gmail.com>
2024-06-17 19:09:12 -0400

    Changing some code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   function sgc() {
     window.open("https://classroom.google.com/c/NjYzNTUwNzIxNjUy?cjc=3szhm7i", "_blank");
   }
-
+  function discord() {
+    window.open("", "_blank");
+  }
   function SlothGoogleClassroom() {
     return (
       <>
@@ -15,12 +17,23 @@ function App() {
       </>
     )
   }
+  function DiscordInvite() {
+    return (
+      <>
+        <button onClick={discord}>rbaxim's discord sever</button>
+      </>
+    )
+  }
   // html
   return (
     <div className="App">
       <header className="App-header">
-          <h1>hi</h1>
+          <h1>Welcome to the Sloth Website</h1>
           <SlothGoogleClassroom />
+          <DiscordInvite />
+          <p>Give me ideas on these pages above</p>
+          <hr />
+          <h6>This is a placeholder for games</h6>
       </header>
     </div>
   );
