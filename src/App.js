d252847ee2ffa7f0c9ebda22c06be3ebfe9c41bd
rbaxim <luc.r.diverse@gmail.com>
2024-08-02 11:29:48 -0400

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Variables
+
+  function sgc() {
+    window.open("https://classroom.google.com/c/NjYzNTUwNzIxNjUy?cjc=3szhm7i", "_blank");
+  }
+
+  function SlothGoogleClassroom() {
+    return (
+      <>
+        <button onClick={sgc}>Sloth Google Classroom</button>
+      </>
+    )
+  }
+  // html
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>hi</h1>
+          <SlothGoogleClassroom />
       </header>
     </div>
   );
 }
+
 
 export default App;
